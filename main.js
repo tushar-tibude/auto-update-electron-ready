b342cf00a294368542f1d67c54d6a51a6aab3053
VELOTIO_WIN_10\Administrator <tushar.tibude@velotio.com>
2020-05-12 17:07:48 +0530

    version updated

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
 const { autoUpdater } = require('electron-updater');
+const log = require('electron-log');
 
 let mainWindow;
 
@@ -12,21 +13,22 @@ function createWindow () {
     },
   });
   mainWindow.loadFile('index.html');
-  mainWindow.on('closed', function () {
-    mainWindow = null;
-  });
+  // mainWindow.on('closed', function () {
+  //   mainWindow = null;
+  // });
     autoUpdater.checkForUpdatesAndNotify();
+    console.log('autoUpdater.checkForUpdatesAndNotify()');
 }
 
 app.on('ready', () => {
   createWindow();
 });
 
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
+// app.on('window-all-closed', function () {
+//   if (process.platform !== 'darwin') {
+//     app.quit();
+//   }
+// });
 
 app.on('activate', function () {
   if (mainWindow === null) {
@@ -39,13 +41,28 @@ ipcMain.on('app_version', (event) => {
 });
 
 autoUpdater.on('update-available', () => {
+  console.log('update-available tt');
   mainWindow.webContents.send('update_available');
 });
 
+autoUpdater.on('download-progress', (progressObj) => {
+  console.log('update-progress tt 1');
+  let log_message = "Download speed: " + progressObj.bytesPerSecond;
+  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+  log.info(log_message);
+  console.info(log_message);
+  mainWindow.webContents.send('message', log_message);
+})
 autoUpdater.on('update-downloaded', () => {
+  console.info('update-downloaded tt');
   mainWindow.webContents.send('update_downloaded');
 });
 
 ipcMain.on('restart_app', () => {
   autoUpdater.quitAndInstall();
+});
+
+autoUpdater.on('error', (error) => {
+  console.info('error', error);
 });
